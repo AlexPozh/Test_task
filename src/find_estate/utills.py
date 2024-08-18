9defeae4d538077bc15ecb50c8da21d6554b9a9f
alexander <87977854+AlexPozh@users.noreply.github.com>
2024-08-18 13:45:49 +0300

    Update utills.py

--- src/find_estate/utills.py
+++ src/find_estate/utills.py
@@ -5,6 +5,6 @@ import time
 import asyncio
 
 async def external_server() -> bool:
-    # await asyncio.sleep(random.randint(1, 60))
+    await asyncio.sleep(random.randint(1, 60))
     return random.choice([True, False])
  
